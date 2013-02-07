16c836d007ab30c7d8a28f2dea5ce0d5acd7fc38
Robin Harrison <merlinc@thecourtsofchaos.com>
2013-02-07 13:12:51 +0000

    Cannibalised previous prototype to get functional track searching. Needs substantial reworking.

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,108 @@
 var sp;
 var models;
-var baseURL = "http://ws.spotify.com/search/1/track.json?q=artist:";
 var search;
 var standardSearchParameters = {"pageSize":3, "searchArtists":false, "searchTracks": true, "searchAlbums":false};
 
-$(document).ready(function () {
+var searchingFestival;
 
-  sp = getSpotifyApi(1);
-  models = sp.require('sp://import/scripts/api/models');
+var artistsData;
+var festivalData;
+var foundTracks = [];
 
-  var artistProcessingQueue = [];
-  var artistData = [];
+function timeFormat(duration) {
+    var seconds = duration / 1000;
+    var minutes = Math.floor(seconds / 60);
+    seconds = seconds % (60);
 
-  // load data
-  $.getJSON('data/festivals.json', function(data) {
-    var festivalItems = [];
-    festivalItems.push('<option value="">Select a festival</option>');
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+}
 
-    $.each(data, function(key, val) {
-      festivalItems.push('<option value="' + val.data + '">' + val.festival + '</option>');
-    });
+
+function runSearch() {
+
+    searchingArtist = artistsData.shift();
+
+    if(searchingArtist) {
+        search = new models.Search('artist:"' + searchingArtist + '"', standardSearchParameters);
+        search.localResults = models.LOCALSEARCHRESULTS.APPEND;
    
-   $('#festivals').empty().append(festivalItems);
-  });
+        var searchHTML = document.getElementById('search');
 
-  // choose a festival
+        search.observe(models.EVENT.CHANGE, function() {
+           foundTracks.concat(search.tracks.slice(0));
+
+            console.log("--- " + searchingArtist + " ---");
+            $('#playlistDisplay > tbody:last').append('<tr scope="row" class="spec"><th colspan="2">' + searchingArtist + '</th></tr>');
+
+            search.tracks.forEach(function(track) {
+                    console.log(track);
+                    $('#playlistDisplay > tbody:last').append('<tr><td>' + track.name + '</td><td>' + timeFormat(track.duration) + '</td></tr>');
+                    foundTracks.push(track);
+                });
+
+            if(artistsData.length) {
+                runSearch();
+            }
+
+        });
+
+        search.appendNext();
+    }
+}
+
+function startSearch(festivalId) {
+
+    foundTracks = [];
+    festivalData = festivalId;
+
+    $.getJSON('sp://festival-generator/data/'+festivalId+'.json', function(data) {
+        artistsData = data;
+        runSearch();
+    });
+
+}
+
+function generatePlaylist() {
+    console.log(foundTracks);
+
+    var newFestivalPlaylist = new models.Playlist("FG - Top 3 Songs");
+    $.each(foundTracks, function(index, value) { newFestivalPlaylist.add(value);});
+}
+
+
+
+function displayResults() {
+    for(i=0;i<foundTracks.length;i++){
+        var link = document.createElement('li');
+        var a = document.createElement('a');
+        a.href = foundTracks[i].uri;
+        link.appendChild(a);
+        a.innerHTML = '<b>' + foundTracks[i].name + '</b> <i>' + foundTracks[i].duration + '</i>';
+        searchHTML.appendChild(link);
+    }         
+}
+
+function initPage() {
+    /* Instantiate the global sp object; include models & views */
+    sp = getSpotifyApi(1);
+    models = sp.require('sp://import/scripts/api/models');
+
+//    $('#generatePlaylist').change(function() { startSearch });
+
+      // load data
+      $.getJSON('data/festivals.json', function(data) {
+        var festivalItems = [];
+        festivalItems.push('<option value="">Select a festival</option>');
+
+        $.each(data, function(key, val) {
+          festivalItems.push('<option value="' + val.data + '">' + val.festival + '</option>');
+        });
+       
+       $('#festivals').empty().append(festivalItems);
+      });
+
+
+      // choose a festival
   $("#festivals").change(function() {
 
     dataURL = $('#festivals option:selected').val();
@@ -46,36 +124,26 @@ $(document).ready(function () {
 
     // choose stages
   $("#stages").change(function() {
-    artistNames = [];
-    artistData = [];
+    artistsData = [];
 
     $.each($("#stages option:selected"), function(key, stage) {
       $.each($(stage).data(), function(key, band) {
-        artistNames.push(band.name);
+        artistsData.push(band.name);
       });
     });
 
-    artistNames.sort();
-    
-    $('#spotify').attr('src', '');
+    artistsData.sort();
 
-    var artistProcessingQueue = new ProcessingQueue();
+    console.log(artistsData);
 
-    artistProcessingQueue.runQueue(artistNames,
-      function(results){
-        artistData = results;
-        /*
-        songIds = [];
-        $.each(artistData, function(key, val) {
-          songIds.push(val.href.slice(14));
-        });
-        console.log(songIds.join(','));
-        spotifyPlaylistURL = "https://embed.spotify.com/?uri=spotify:trackset:Playlist:"+songIds.join(',');
-        $('#spotify').attr('src', spotifyPlaylistURL);
-        */
-      },
-      function(error){
-        console.log(error);
+    runSearch();
     });
-  });
-});
+
+    $('#generatePlaylist').click(function() {generatePlaylist();});
+
+//$("#target").click(function() {
+ // alert("Handler for .click() called.");
+//});
+
+}
+$(document).ready( initPage() );
